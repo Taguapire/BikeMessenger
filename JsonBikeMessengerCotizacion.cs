210aa93d173636ea663aee2dbae41beffec8a702
Luis Vasquez <dressofshadows@hotmail.com>
2022-03-01 01:55:20 -0300

    Cotizacion Base de Datos

--- JsonBikeMessengerCotizacion.cs
+++ JsonBikeMessengerCotizacion.cs
@@ -1,12 +1,177 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace BikeMessenger
 {
-    internal class JsonBikeMessengerCotizacion
+    internal class StructBikeMessengerCotizacion
     {
+        public string OPERACION { get; set; }
+        public string PKCOTIZACION { get; set; }
+        public string PENTALPHA { get; set; }
+        public string COTIZACION { get; set; }
+        public string NOMBRE { get; set; }
+        public string TELEFONO { get; set; }
+        public string EMAIL { get; set; }
+        public string OCALLE { get; set; }
+        public string ONUMERO { get; set; }
+        public string ODPTO { get; set; }
+        public string OOFICINA { get; set; }
+        public string OCASA { get; set; }
+        public string OCOMUNA { get; set; }
+        public string ONOMBRE { get; set; }
+        public string OTELEFONO { get; set; }
+        public string DCALLE { get; set; }
+        public string DNUMERO { get; set; }
+        public string DDPTO { get; set; }
+        public string DOFICINA { get; set; }
+        public string DCASA { get; set; }
+        public string DCOMUNA { get; set; }
+        public string DNOMBRE { get; set; }
+        public string DTELEFONO { get; set; }
+        public string REGRESODE { get; set; }
+        public string TIPOCARGA { get; set; }
+        public double LARGO { get; set; }
+        public double ANCHO { get; set; }
+        public double ALTO { get; set; }
+        public double PESO { get; set; }
+        public string DESCRIPCION { get; set; }
+        public string FECHAENTREGA { get; set; }
+        public string HORAENTREGA { get; set; }
+        public double DISTANCIA { get; set; }
+        public string RESOPERACION { get; set; }
+        public string RESMENSAJE { get; set; }
+
+        public StructBikeMessengerCotizacion()
+        {
+            OPERACION = "";
+            PKCOTIZACION = "";
+            PENTALPHA = "";
+            COTIZACION = "";
+            NOMBRE = "";
+            TELEFONO = "";
+            EMAIL = "";
+            OCALLE = "";
+            ONUMERO = "";
+            ODPTO = "";
+            OOFICINA = "";
+            OCASA = "";
+            OCOMUNA = "";
+            ONOMBRE = "";
+            OTELEFONO = "";
+            DCALLE = "";
+            DNUMERO = "";
+            DDPTO = "";
+            DOFICINA = "";
+            DCASA = "";
+            DCOMUNA = "";
+            DNOMBRE = "";
+            DTELEFONO = "";
+            REGRESODE = "";
+            TIPOCARGA = "";
+            LARGO = 0;
+            ANCHO = 0;
+            ALTO = 0;
+            PESO = 0;
+            DESCRIPCION = "";
+            FECHAENTREGA = "";
+            HORAENTREGA = "";
+            DISTANCIA = 0;
+            RESOPERACION = "";
+            RESMENSAJE = "";
+        }
+    }
+
+    internal class TbBikeMessengerCotizacion
+    {
+        [Column("operacion")]
+        public string OPERACION { get; set; }
+        [PrimaryKey]
+        [Column("pkcotizacion")]
+        public string PKCOTIZACION { get; set; }
+        [Column("pentalpha")]
+        public string PENTALPHA { get; set; }
+        [Column("cotizacion")]
+        public string COTIZACION { get; set; }
+        [Column("nombre")]
+        public string NOMBRE { get; set; }
+        [Column("telefono")]
+        public string TELEFONO { get; set; }
+        [Column("email")]
+        public string EMAIL { get; set; }
+        [Column("ocalle")]
+        public string OCALLE { get; set; }
+        [Column("onumero")]
+        public string ONUMERO { get; set; }
+        [Column("odpto")]
+        public string ODPTO { get; set; }
+        [Column("ooficina")]
+        public string OOFICINA { get; set; }
+        [Column("ocasa")]
+        public string OCASA { get; set; }
+        [Column("ocomuna")]
+        public string OCOMUNA { get; set; }
+        [Column("onombre")]
+        public string ONOMBRE { get; set; }
+        [Column("otelefono")]
+        public string OTELEFONO { get; set; }
+        [Column("dcalle")]
+        public string DCALLE { get; set; }
+        [Column("dnumero")]
+        public string DNUMERO { get; set; }
+        [Column("ddpto")]
+        public string DDPTO { get; set; }
+        [Column("doficina")]
+        public string DOFICINA { get; set; }
+        [Column("dcasa")]
+        public string DCASA { get; set; }
+        [Column("dcomuna")]
+        public string DCOMUNA { get; set; }
+        [Column("dnombre")]
+        public string DNOMBRE { get; set; }
+        [Column("dtelefono")]
+        public string DTELEFONO { get; set; }
+        [Column("regresode")]
+        public string REGRESODE { get; set; }
+        [Column("tipocarga")]
+        public string TIPOCARGA { get; set; }
+        [Column("largo")]
+        public double LARGO { get; set; }
+        [Column("ancho")]
+        public double ANCHO { get; set; }
+        [Column("alto")]
+        public double ALTO { get; set; }
+        [Column("peso")]
+        public double PESO { get; set; }
+        [Column("descripcion")]
+        public string DESCRIPCION { get; set; }
+        [Column("fechaentrega")]
+        public string FECHAENTREGA { get; set; }
+        [Column("horaentrega")]
+        public string HORAENTREGA { get; set; }
+        [Column("distancia")]
+        public double DISTANCIA { get; set; }
+        [Column("resoperacion")]
+        public string RESOPERACION { get; set; }
+        [Column("resmensaje")]
+        public string RESMENSAJE { get; set; }
+    }
+
+    internal class TbVistaCotizacionCliMen
+    {
+        [Column("cotizacion")]
+        public string COTIZACION { get; set; }
+        [Column("nombre")]
+        public string NOMBRE { get; set; }
+        [Column("tipocarga")]
+        public string TIPOCARGA { get; set; }
+        [Column("origen")]
+        public string ORIGEN { get; set; }
+        [Column("destino")]
+        public string DESTINO { get; set; }
+        [Column("fechaentrega")]
+        public string FECHAENTREGA { get; set; }
+        [Column("horaentrega")]
+        public string HORAENTREGA { get; set; }
+        [Column("distancia")]
+        public double DISTANCIA { get; set; }
     }
 }
--- PageCotizacion.xaml.cs
+++ PageCotizacion.xaml.cs
@@ -26,6 +26,8 @@ namespace BikeMessenger
     {
         private TransferVar LvrTransferVar = new TransferVar();
         private bool ContinuarSiNo;
+        private readonly Uri PaginaCotizacion = new Uri("ms-appx-web:///html/EditorCotizaciones.html");
+        private StructBikeMessengerCotizacion DbCotizacion = new StructBikeMessengerCotizacion();
 
         public PageCotizacion()
         {
@@ -132,6 +134,133 @@ namespace BikeMessenger
 
         private void RevisarDetalleCotizacionServicio(object sender, Windows.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
         {
+            VisorDetalleCotizaciones.Visibility = Visibility.Visible;
+            DGViewCotizacion.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnNuevaCotizacion(object sender, RoutedEventArgs e)
+        {
+            VisorDetalleCotizaciones.Visibility = Visibility.Visible;
+            DGViewCotizacion.Visibility = Visibility.Collapsed;
+            try
+            {
+                VisorDetalleCotizaciones.Navigate(PaginaCotizacion);
+            }
+            catch (System.ArgumentException)
+            {
+                ;
+            }
+            catch (System.Exception)
+            {
+                ;
+            }
+        }
+
+        private void BtnEditarCotizacion(object sender, RoutedEventArgs e)
+        {
+            VisorDetalleCotizaciones.Visibility = Visibility.Visible;
+            DGViewCotizacion.Visibility = Visibility.Collapsed;
+            try
+            {
+                VisorDetalleCotizaciones.Navigate(PaginaCotizacion);
+            }
+            catch (System.ArgumentException)
+            {
+                ;
+            }
+            catch (System.Exception)
+            {
+                ;
+            }
+        }
+
+        private void BtnEliminarCotizacion(object sender, RoutedEventArgs e)
+        {
+            VisorDetalleCotizaciones.Visibility = Visibility.Visible;
+            DGViewCotizacion.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnListaDeCotizaciones(object sender, RoutedEventArgs e)
+        {
+            DGViewCotizacion.Visibility = Visibility.Visible;
+            VisorDetalleCotizaciones.Visibility = Visibility.Collapsed;
+        }
+
+        private void VisorCotizacionScriptNotify(object sender, NotifyEventArgs e)
+        {
+            string RetornoCotizacion = e.Value;
+            string[] ParametrosCotizacion = RetornoCotizacion.Split("&&");
+
+            DbCotizacion.OPERACION = "ACTUALIZAR";
+            DbCotizacion.PENTALPHA = LvrTransferVar.PENTALPHA_ID + "1";
+            DbCotizacion.PENTALPHA = LvrTransferVar.PENTALPHA_ID;
+            DbCotizacion.COTIZACION = "1";
+            DbCotizacion.NOMBRE = ParametrosCotizacion[0];
+            DbCotizacion.TELEFONO = ParametrosCotizacion[1];
+            DbCotizacion.EMAIL = ParametrosCotizacion[2];
+            DbCotizacion.OCALLE = ParametrosCotizacion[3];
+            DbCotizacion.ONUMERO = ParametrosCotizacion[4];
+            DbCotizacion.ODPTO = ParametrosCotizacion[5];
+            DbCotizacion.OOFICINA = ParametrosCotizacion[6];
+            DbCotizacion.OCASA = ParametrosCotizacion[7];
+            DbCotizacion.OCOMUNA = ParametrosCotizacion[8];
+            DbCotizacion.ONOMBRE = ParametrosCotizacion[9];
+            DbCotizacion.OTELEFONO = ParametrosCotizacion[10];
+            DbCotizacion.DCALLE = ParametrosCotizacion[11];
+            DbCotizacion.DNUMERO = ParametrosCotizacion[12];
+            DbCotizacion.DDPTO = ParametrosCotizacion[13];
+            DbCotizacion.DOFICINA = ParametrosCotizacion[14];
+            DbCotizacion.DCASA = ParametrosCotizacion[15];
+            DbCotizacion.DCOMUNA = ParametrosCotizacion[16];
+            DbCotizacion.DNOMBRE = ParametrosCotizacion[17];
+            DbCotizacion.DTELEFONO = ParametrosCotizacion[18];
+            DbCotizacion.REGRESODE = ParametrosCotizacion[19];
+            DbCotizacion.TIPOCARGA = ParametrosCotizacion[20];
+            DbCotizacion.LARGO = Double.Parse(ParametrosCotizacion[21]);
+            DbCotizacion.ANCHO = Double.Parse(ParametrosCotizacion[22]);
+            DbCotizacion.ALTO = Double.Parse(ParametrosCotizacion[23]);
+            DbCotizacion.PESO = Double.Parse(ParametrosCotizacion[24]);
+            DbCotizacion.DESCRIPCION = ParametrosCotizacion[25];
+            DbCotizacion.FECHAENTREGA = ParametrosCotizacion[26];
+            DbCotizacion.HORAENTREGA = ParametrosCotizacion[27];
+            DbCotizacion.DISTANCIA = Double.Parse(ParametrosCotizacion[28]);
+            DbCotizacion.RESOPERACION = "OK";
+            DbCotizacion.RESMENSAJE = "OK";
+        }
+
+        private async void VisorCotizacionLlenadoFormulario(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            string[] Parametros = new string[29];
+            Parametros[0] = DbCotizacion.NOMBRE ?? "";
+            Parametros[1] = DbCotizacion.TELEFONO ?? "";
+            Parametros[2] = DbCotizacion.EMAIL ?? "";
+            Parametros[3] = DbCotizacion.OCALLE ?? "";
+            Parametros[4] = DbCotizacion.ONUMERO ?? "";
+            Parametros[5] = DbCotizacion.ODPTO ?? "";
+            Parametros[6] = DbCotizacion.OOFICINA ?? "";
+            Parametros[7] = DbCotizacion.OCASA ?? "";
+            Parametros[8] = DbCotizacion.OCOMUNA ?? "";
+            Parametros[9] = DbCotizacion.ONOMBRE ?? "";
+            Parametros[10] = DbCotizacion.OTELEFONO ?? "";
+            Parametros[11] = DbCotizacion.DCALLE ?? "";
+            Parametros[12] = DbCotizacion.DNUMERO ?? "";
+            Parametros[13] = DbCotizacion.DDPTO ?? "";
+            Parametros[14] = DbCotizacion.DOFICINA ?? "";
+            Parametros[15] = DbCotizacion.DCASA ?? "";
+            Parametros[16] = DbCotizacion.DCOMUNA ?? "";
+            Parametros[17] = DbCotizacion.DNOMBRE ?? "";
+            Parametros[18] = DbCotizacion.DTELEFONO ?? "";
+            Parametros[19] = DbCotizacion.REGRESODE ?? "NO";
+            Parametros[20] = DbCotizacion.TIPOCARGA ?? "OTROS";
+            Parametros[21] = DbCotizacion.LARGO.ToString() ?? "0";
+            Parametros[22] = DbCotizacion.ANCHO.ToString() ?? "0";
+            Parametros[23] = DbCotizacion.ALTO.ToString() ?? "0";
+            Parametros[24] = DbCotizacion.PESO.ToString() ?? "0";
+            Parametros[25] = DbCotizacion.DESCRIPCION ?? "";
+            Parametros[26] = DbCotizacion.FECHAENTREGA ?? "";
+            Parametros[27] = DbCotizacion.HORAENTREGA ?? "";
+            Parametros[28] = DbCotizacion.DISTANCIA.ToString() ?? "0";
+            _ = await VisorDetalleCotizaciones.InvokeScriptAsync("llenadoFormulario", Parametros);
         }
     }
 
